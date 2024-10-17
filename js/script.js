47224a44fb4ea994a105107b3953ccb52f130c89
Bruno Carletto Adamczyk <150372999+BruDu1545@users.noreply.github.com>
2024-10-17 12:03:44 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
+    
+    const savedEmail = "Bruno";
+    const savedPassword = "CavaScript";
+
+    if (email === savedEmail && password === savedPassword) {
+        alert(`Welcome back, ${email}!`);
+        window.location.href = 'config/dashboard.html';
+    } else {
+        alert("Invalid email or password. Please try again.");
+    }
+});
+
+/*document.getElementById("btn-2").addEventListener("click", function() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    if (email && password) {
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+        alert("Sign up successful! You can now log in.");
+    } else {
+        alert("Please fill in both email and password fields.");
+    }
+});*/
